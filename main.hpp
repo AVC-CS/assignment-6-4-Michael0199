fdd67bbe32f58442ead5faae63d2a2ab23a23a54
Michael Hodges <156959963+Michael0199@users.noreply.github.com>
2024-03-13 20:30:35 +0000

    the voideronis

--- main.hpp
+++ main.hpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
-int swapValues(int &, int &);
-int swapValues(int &, int &, int &);
-int swapValues(int &, int &, int &, int &);
+void swapValues(int &, int &);
+void swapValues(int &, int &, int &);
+void swapValues(int &, int &, int &, int &);
 
-int swapValues(int &a, int &b){
+void swapValues(int &a, int &b){
   int x=a;
   a=b;
   b=x;
-  return a,b;
 }
-int swapValues(int &a, int &b, int &c){
+void swapValues(int &a, int &b, int &c){
   int x=a;
   a=b;
   b=c;
   c=x;
-  return a,b,c;
 }
-int swapValues(int &a, int &b, int &c, int &d){
+void swapValues(int &a, int &b, int &c, int &d){
   int x=a;
   int z=b;
   a=d;
   b=c;
   c=z;
   d=x;
-  return a,b,c,d;
 }
